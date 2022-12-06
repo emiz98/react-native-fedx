dfe9fe3058dec54040bbfd39df5f3a852193071e
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2022-12-07 02:34:08 +0530

    nativewind init

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex items-center justify-center h-full bg-blue-500">
+      <Text className='text-white font-medium text-lg'>Welcome to FedX Clone</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
